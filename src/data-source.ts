5842b69b26e42f3649278cdc429ad7ae2e3e9cb2
mnmilkim@gmail.com <mnmilkim@gmail.com>
2024-08-01 12:19:30 +0800

    Update generate database automatically

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import * as dotenv from "dotenv"
+import { Client } from "pg"
 dotenv.config()
 
 const TypeORMConfig: Record<string, DataSource> = {
@@ -60,12 +61,39 @@ const TypeORMConfig: Record<string, DataSource> = {
 
 export const AppDataSource = TypeORMConfig[process.env.NODE_ENV ?? "development"];
 
+export const createDBIfNotExists = async (AppDataSource: DataSource): Promise<void> => {
+    const client = new Client({
+        user: process.env.DB_USERNAME || "postgres",
+        host: process.env.DB_HOST || "localhost",
+        password: process.env.DB_PASSWORD || "postgres",
+        port: Number(process.env.DB_PORT) || 5432,
+    });
+
+    await client.connect();
+
+    const res = await client.query(`SELECT datname FROM pg_catalog.pg_database WHERE datname = '${AppDataSource.options.database}';`);
+
+    if (res.rowCount === 0) {
+        console.log(`${AppDataSource.options.database} database not found, creating it.`);
+        await client.query(`CREATE DATABASE "${AppDataSource.options.database}";`);
+        console.log(`created database ${AppDataSource.options.database}`);
+    } else {
+        console.log(`${AppDataSource.options.database} database exists.`);
+    }
+
+    await client.end();
+}
+
 export const createTypeormConnection = async () => {
+    await createDBIfNotExists(AppDataSource);
+
     await AppDataSource.initialize()
         .then(async (conn) => {
-            await conn.runMigrations();
             console.log('Database connected successfully');
-            console.log('Migration run successfully');
         }).catch(error => console.log(error))
+
+    await AppDataSource.runMigrations()
+        .catch(error => console.log(error));
+    console.log('Migration run successfully');
     return AppDataSource;
 }
